a7fafe0b2a4ae1da040a131bcdd4fed50aab3511
Carlos Afonso <caafonso@deloitte.pt>
2025-07-26 12:00:39 +0100

    HB-20 Define rescues for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { error: 'Resource not found.' }, status: :not_found
+  end
+
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { error: exception.message }, status: :bad_request
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    render json: { message: exception.message }, status: :unprocessable_content
+  end
 end
